abd2f6c3b3c00bd2b8b825dca6527de1607a4c1e
Tim Gray <kc4ums@yahoo.com>
2025-03-03 10:35:52 -0500

    Create ad9833_test.ino

---
+++ ad9833_test.ino
@@ -0,0 +1,8 @@
+// Set specific frequency and phase
+setFrequency(1000);  // 1kHz
+setPhase(90);        // 90 degrees phase shift
+
+// Use different phase angles
+setPhase(0);    // 0 degrees
+setPhase(180);  // 180 degrees
+setPhase(270);  // 270 degrees
